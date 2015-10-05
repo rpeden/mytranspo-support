0fc94a9025401e9fcc302e0381bbc5215de0cafa
Ryan Peden <ryan04@gmail.com>
2015-10-05 17:47:14 -0400

    make sqlite default database

--- create-database.rb
+++ create-database.rb
@@ -3,21 +3,21 @@ require 'rubygems'
 require 'sequel'
 
 
-#DB = Sequel.connect('sqlite://transpo.db')
+DB = Sequel.connect('sqlite://transpo.db')
 
-#DB.execute("PRAGMA default_cache_size = 200000")
-#DB.execute("PRAGMA default_synchronous = OFF")
-#DB.execute("PRAGMA journal_mode = OFF")
-#DB.execute("PRAGMA temp_store = MEMORY")
+DB.execute("PRAGMA default_cache_size = 200000")
+DB.execute("PRAGMA default_synchronous = OFF")
+DB.execute("PRAGMA journal_mode = OFF")
+DB.execute("PRAGMA temp_store = MEMORY")
 
-DB = Sequel.connect(:adapter=>'mysql', :socket=>'/var/run/mysqld/mysqld.sock', :database=>'transpo', :user=>'', :password=>'')
-    
+#DB = Sequel.connect(:adapter=>'mysql', :socket=>'/var/run/mysqld/mysqld.sock', :database=>'transpo', :user=>'', :password=>'')
+
     if(DB.table_exists? :stops) then DB.drop_table :stops end
     if(DB.table_exists? :trips) then DB.drop_table :trips end
     if(DB.table_exists? :routes) then DB.drop_table :routes end
     if(DB.table_exists? :stop_times) then DB.drop_table :stop_times end
-    
-                    
+
+
     DB.create_table :stops do
       column(:stop_id, :varchar, {:size => 5, :auto_increment => false, :unique =>true, :null => :false})
       column(:stop_name, :varchar, {:size => 50, :null => false})
@@ -26,7 +26,7 @@ DB = Sequel.connect(:adapter=>'mysql', :socket=>'/var/run/mysqld/mysqld.sock', :
       primary_key(:stop_id)
       index([:stop_id, :stop_lat, :stop_lon])
     end
-    
+
     DB.create_table :trips do
       column(:trip_id, :varchar, {:size => 50, :auto_increment => false, :unique => true, :null => false})
       column(:route_id, :varchar, {:size => 40, :null => false})
@@ -42,7 +42,7 @@ DB = Sequel.connect(:adapter=>'mysql', :socket=>'/var/run/mysqld/mysqld.sock', :
       column(:route_short_name, :varchar, {:size => 5, :null => false})
       primary_key(:route_id)
     end
-    
+
     DB.create_table :stop_times do
       column(:stop_id, :varchar, {:size => 10, :null => false})
       column(:trip_id, :varchar, {:size => 50, :null => false})
@@ -50,4 +50,3 @@ DB = Sequel.connect(:adapter=>'mysql', :socket=>'/var/run/mysqld/mysqld.sock', :
       primary_key(:stop_id)
       index([:stop_id, :departure_time])
     end
-
